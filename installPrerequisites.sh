725c380999ff526680ca1db4c0908d38825a27b9
Mphasis <karthik.ganti@mphasis.com>
2019-07-15 11:54:09 +0530

    Initial commit

---
+++ installPrerequisites.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+### Install vagrant for debian 64bit system
+wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.deb
+sudo dpkg -i vagrant_2.2.5_x86_64.deb
+
+### Install oracle virtual box for debian 64 bit
